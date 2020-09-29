e78f320ebab845e218b54d883a85389e9a6ac9cb
Đăng Khoa <72064370+DangKhoa09@users.noreply.github.com>
2020-09-29 14:34:10 +0700

    Add files via upload

---
+++ bai1.java
@@ -0,0 +1,20 @@
+package BaiTap;
+
+import java.util.Scanner;
+
+public class bai1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap a: ");
+		int a= sc.nextInt();
+		System.out.println("Nhap b: ");
+		int b= sc.nextInt();
+		int kq=a+b;
+		System.out.println("Tong a + b ="+kq);
+		
+		
+
+	}
+
+}
